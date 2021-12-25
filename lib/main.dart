6c3ae7c394b13270bdffa46f91232558003c13ee
Alejandro Saucedo <axsauze@gmail.com>
2021-12-25 09:00:43 +0000

    Added initial view

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import "./dummy_data.dart";
 // import "./screens/tabs_screen.dart";
 // import "./screens/meal_detail_screen.dart";
-// import "./screens/category_meals_screen.dart";
+import "./screens/categories_screen.dart";
 // import "./screens/filters_screen.dart";
 import "./models/meal.dart";
 
@@ -59,23 +59,25 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "DeliMeals",
-      // theme: ThemeData(
-      //   primarySwatch: Colors.pink,
-      //   accentColor: Colors.amber,
-      //   //fontFamily: "Raleway",
-      //   textTheme: ThemeData.light().textTheme.copyWith(
-      //     bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-      //     bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-      //     headline1: TextStyle(
-      //       fontSize: 20,
-      //       //fontFamily: "RobotoCondensed",
-      //       fontWeight: FontWeight.bold,
-      //     ),
-      //   ),
-      //),
-      home: Text("Hello"),
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        colorScheme: Theme.of(context).colorScheme.copyWith(secondary: Colors.amber),
+        fontFamily: "Raleway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          headline1: const TextStyle(
+            fontSize: 20,
+            fontFamily: "RobotoCondensed",
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        body: CategoriesScreen(),
+      ),
     );
   }
 
